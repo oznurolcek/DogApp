147b2fcfad82b8b404064256b02490bf380d21b6
oznurolcek <olcekoznur@gmail.com>
2023-11-22 20:13:12 +0300

    gridview added

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,80 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final List<Map> myDogs = List.generate(
+      100,
+      (index) => {
+            "id": index,
+            "name": "Dog $index",
+            "imageUrl":
+                "https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg",
+            "breed": "Breed $index"
+          }).toList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Dog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5,
+            childAspectRatio: 1,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          itemCount: myDogs.length,
+          itemBuilder: (_, index) {
+            return Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                    image: DecorationImage(
+                      image: NetworkImage(myDogs[index]["imageUrl"]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0.0,
+                  left: 0.0,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomLeft: Radius.circular(20)),
+                      color: Colors.black.withOpacity(0.3),
+                    ),
+                    child: ClipRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                        child: Center(
+                          child: Text(
+                            myDogs[index]["breed"],
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
       ),
     );
   }
